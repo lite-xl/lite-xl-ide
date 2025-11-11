9fa99660f4d67bcb2942b969e0642a2eb344f464
Adam Harrison <adamdharrison@gmail.com>
2025-11-10 22:02:21 -0500

    w64 update.

--- plugins/w64devkit.lua
+++ plugins/w64devkit.lua
@@ -1,9 +1,13 @@
 --mod-version:4
+local config = require "core.config"
 
-if PLATFORM == "windows" then
-  local w64devkitpath = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "w64devkit"
+if PLATFORM == "Windows" then
+  local w64devkitpath = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "w64devkit" .. PATHSEP .. "w64devkit"
   config.plugins.terminal.shell = w64devkitpath .. PATHSEP .. "bin" .. PATHSEP .. "sh.exe"
   config.plugins.build.cc = w64devkitpath .. PATHSEP .. "bin" .. PATHSEP .. "gcc.exe"
   config.plugins.build.cxx = w64devkitpath .. PATHSEP .. "bin" .. PATHSEP .. "g++.exe"
   config.plugins.build.ar = w64devkitpath .. PATHSEP .. "bin" .. PATHSEP .. "ar.exe"
+  config.plugins.terminal.environment = {
+    PS1 = '$PWD$ '
+  }
 end
